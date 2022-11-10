e6e40554166b4bbabf63c209603516c8aaf50cfe
prince <princehenry40@gmail.com>
2022-11-10 03:59:51 +0000

    commit is complete

---
+++ challenge3homework.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script:                       
+# Author:                       
+# Date of latest revision:      
+# Purpose:      
+
+# Basic function
+print_something () {
+  echo Hello I am a function
+}
+print_something
+print_something
+
+box=last
+$box
